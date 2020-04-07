2373e3008d4afa20b9243432e3cfff9459deed26
Tom <thomas.spencer@codurance.com>
2020-04-07 17:58:42 +0100

    Add separate getRoman method for adding Roman string to stringBuilder().

--- src/main/java/com/codurance/roman_numerals/RomanNumerals.java
+++ src/main/java/com/codurance/roman_numerals/RomanNumerals.java
@@ -5,18 +5,23 @@ public class RomanNumerals {
     StringBuilder roman = new StringBuilder();
 
     while(arabic > 0){
-      if (arabic >= 5){
-        roman.append("V");
-        arabic -= 5;
-      }else if(arabic >= 4){
-        roman.append("IV");
-        arabic -= 4;
-      } else{
-        roman.append("I");
-        arabic--;
-      }
+      arabic = getRoman(arabic, roman);
     }
 
     return roman.toString();
+  }
+
+  private int getRoman(int arabic, StringBuilder roman) {
+    if (arabic >= 5){
+      roman.append("V");
+      arabic -= 5;
+    }else if(arabic >= 4){
+      roman.append("IV");
+      arabic -= 4;
+    } else{
+      roman.append("I");
+      arabic--;
+    }
+    return arabic;
   }
 }
